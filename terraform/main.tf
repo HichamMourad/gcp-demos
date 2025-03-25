1c572184a7298b8e86fbd70615a496eb68c8ed44
Hicham Mourad <hicham.mourad@gmail.com>
2025-03-25 14:00:23 -0400

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-# Output the VM's public IP
-output "vm_public_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
-}
+## Output the VM's public IP
+# output "vm_public_ip" {
+#   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+# }
